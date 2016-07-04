00dff145b6bf72fcf8ae5f79094dcadd6ae15788
kisphp <mariusbogdan83@gmail.com>
2016-07-04 06:43:43 +0000

    added contact page

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('noPhoneWeek', ['ui.router'])
+var app = angular.module('noPhoneWeek', ['ui.router'])
 .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
   // Fallback
@@ -19,8 +19,16 @@ angular.module('noPhoneWeek', ['ui.router'])
       url: '/register',
       templateUrl: '/dist/templates/user-register.html'
     })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: '/dist/templates/contact.html'
+    })
     .state('recover', {
       url: '/recover',
       templateUrl: '/dist/templates/user-recover.html'
     });
+}]);
+
+app.controller('ContactController', ['$scope', function($scope){
+  $scope.contactUrl = 'https://formspree.io/your@email.com';
 }]);
